3f50fbdcbac8b0a4bd6e4d22c66fa7067c540297
minhduc <minhduc4600@gmail.com>
2021-08-18 16:06:22 +0700

    firebase code

--- src/main/java/com/example/testgit/Controller.java
+++ src/main/java/com/example/testgit/Controller.java
@@ -2,20 +2,20 @@ package com.example.testgit;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@org.springframework.stereotype.Controller
+//@org.springframework.stereotype.Controller
 public class Controller {
-    @RequestMapping("/")
-    public String index(){
-        return "index";
-    }
-    @RequestMapping("/")
-    public String e(){
-        return "index";
-    }
-    @RequestMapping("/")
-    public String eee(){
-        return "index";
-    }
+//    @RequestMapping("/")
+//    public String index(){
+//        return "index";
+//    }
+//    @RequestMapping("/")
+//    public String e(){
+//        return "index";
+//    }
+//    @RequestMapping("/")
+//    public String eee(){
+//        return "index";
+//    }
     //dhcjhsgcu
     //day
 
--- src/main/java/com/example/testgit/entity/Product.java
+++ src/main/java/com/example/testgit/entity/Product.java
@@ -4,5 +4,27 @@ public class Product {
     private String name;
     private int price;
 
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
