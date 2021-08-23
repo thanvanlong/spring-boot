8cb6df08187fef85075505db1cd8c85e3e8d4298
thanvanlong <72558875+thanvanlong@users.noreply.github.com>
2021-08-23 18:18:38 +0700

    ok

--- src/main/java/com/example/testgit/controller/RegistrationController.java
+++ src/main/java/com/example/testgit/controller/RegistrationController.java
@@ -6,17 +6,25 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @AllArgsConstructor
 public class RegistrationController {
     private final RegistrationService registrationService;
 
-    @ResponseBody
     @PostMapping("/register")
-    public String register(@RequestBody RegistrationRequest registrationRequest){
+    public String register(HttpServletRequest httpServletRequest)  {
+        String firstName = httpServletRequest.getParameter("firstName");
+        String lastName = httpServletRequest.getParameter("lastName");
+        String password = httpServletRequest.getParameter("password");
+        String email = httpServletRequest.getParameter("email");
+        RegistrationRequest request = new RegistrationRequest(firstName,lastName,password,email);
+        System.out.println(request.getFirstName());
+        System.out.println(registrationService.register(request));
 
-        registrationService.register(registrationRequest);
-        return "confirmEmail";
+
+        return "confirm";
     }
 
     @GetMapping("/login")
