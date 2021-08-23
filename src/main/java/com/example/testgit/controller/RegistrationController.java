f1d3fd0cfd45c2659ce0cc81532d2d79c27d73a7
minhduc <minhduc4600@gmail.com>
2021-08-23 18:32:02 +0700

    s

--- src/main/java/com/example/testgit/controller/RegistrationController.java
+++ src/main/java/com/example/testgit/controller/RegistrationController.java
@@ -14,12 +14,12 @@ public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping("/register")
-    public String register(HttpServletRequest httpServletRequest)  {
+    public String register(HttpServletRequest httpServletRequest) {
         String firstName = httpServletRequest.getParameter("firstName");
         String lastName = httpServletRequest.getParameter("lastName");
         String password = httpServletRequest.getParameter("password");
         String email = httpServletRequest.getParameter("email");
-        RegistrationRequest request = new RegistrationRequest(firstName,lastName,password,email);
+        RegistrationRequest request = new RegistrationRequest(firstName, lastName, password, email);
         System.out.println(request.getFirstName());
         System.out.println(registrationService.register(request));
         //say ok
